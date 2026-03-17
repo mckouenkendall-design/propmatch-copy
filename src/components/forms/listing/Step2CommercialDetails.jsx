ab6fb21e7d8cdd991ed0d6d03f8acd7ac31943a9
Mckouen Kendall <mckouenkendall@gmail.com>
2026-03-17 02:29:14 +0000

    File changes

--- src/components/forms/listing/Step2CommercialDetails.jsx
+++ src/components/forms/listing/Step2CommercialDetails.jsx
@@ -1,5 +1,4 @@
 import React, { useRef } from 'react';
-import React, { useRef } from 'react';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
