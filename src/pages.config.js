0af015f0935cd2e19e08b39a103ddddbdae2c05a
Mckouen Kendall <mckouenkendall@gmail.com>
2026-03-19 02:44:08 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,14 +1,4 @@
 import Dashboard from './pages/Dashboard';
-import Dealboard from './pages/Dealboard';
-import Listings from './pages/Listings';
-import Matches from './pages/Matches';
-import Messages from './pages/Messages';
-import MyPosts from './pages/MyPosts';
-import Requirements from './pages/Requirements';
-import MyTemplates from './pages/MyTemplates';
-import Groups from './pages/Groups';
-import GroupDetail from './pages/GroupDetail';
-import __Layout from './Layout.jsx';
 
 export const PAGES = {
     "Dashboard": Dashboard,
