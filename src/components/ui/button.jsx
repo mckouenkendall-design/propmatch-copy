7d8efd2062687132aee36ccfb0ba05dbf3d4d4ae
Mckouen Kendall <mckouenkendall@gmail.com>
2026-03-20 05:01:34 +0000

    File changes

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -14,10 +14,10 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background shadow-sm hover:bg-[rgba(0,219,197,0.08)] hover:text-[#00DBC5]",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-[rgba(0,219,197,0.08)] hover:text-[#00DBC5]",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
